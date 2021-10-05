d56700fdfa8cc4754f38cfc3446579f8e0f64b1a
NataliaTepluhina <tarya.se@gmail.com>
2021-10-05 19:43:29 +0200

    Fixed api

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 import { createApp, h } from 'vue'
-import { useQueryProvider } from 'vue-query'
+import { useQueryProvider, QueryClient } from 'vue-query'
 import router from './router'
 import App from './App.vue'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5000,
+    },
+  },
+})
 
 createApp({
   render() {
     return h(App)
   },
   setup() {
-    useQueryProvider()
+    useQueryProvider(queryClient)
   },
 })
   .use(router)
